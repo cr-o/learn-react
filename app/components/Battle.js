305cc68b340277e863e786e13abe6320eb11aef3
catbeans <workemailzoop@gmail.com>
2020-03-16 16:13:31 -0400

    Results Skeleton

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions(){
     return(
@@ -118,7 +119,8 @@ export default class Battle extends React.Component{
 
         this.state = {
             playerOne: null,
-            playerTwo: null
+            playerTwo: null,
+            battle: false
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -135,7 +137,13 @@ export default class Battle extends React.Component{
         })
     }
     render(){
-        const { playerOne, playerTwo } = this.state
+        const { playerOne, playerTwo, battle } = this.state
+
+        if(battle === true){
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+            // results will receive those players as props and then it will fetch the info from the gitHub API
+        }
+
         return (
             <React.Fragment>
                 <Instructions/>
@@ -166,6 +174,15 @@ export default class Battle extends React.Component{
                             />
                         }
                     </div>
+                    {/* we want to show the button when both playerOne and playerTwo are not null */}
+                    {playerOne && playerTwo && (
+                        <button
+                            className='btn dark-btn btn-space'
+                            onClick={()=>this.setState({battle: true})}
+                        >
+                            Battle
+                        </button>
+                    )}
                 </div>
             </React.Fragment>
         )
