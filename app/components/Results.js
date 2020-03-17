8937cebddcb10787c4fe3c54da2f4286973368e6
catbeans <workemailzoop@gmail.com>
2020-03-17 12:51:47 -0400

    Results Data

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,16 @@ import React from 'react'
 import {battle} from '../utils/api' // named import since function wasn't a default export
 
 export default class Results extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
     componentDidMount(){
         const {
             playerOne,
@@ -10,14 +20,24 @@ export default class Results extends React.Component {
 
         battle([playerOne, playerTwo]) // invoke battle with an array of players
         .then((players)=> {
-            console.log('data: ', players)
+            this.setState({
+                winner: players[0],
+                loser: players[1],
+                error: null, // this would be a successful reqiest
+                loading: false
+            })
+        }).catch(({message})=>{ // destructure the message that we're getting form the error
+            this.setState({
+                error: message,
+                loading: false
+            })
         })
     }
     render(){
         return(
             <div>
                 Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
+                <pre>{JSON.stringify(this.state, null, 2)}</pre>
             </div>
         )
     }
