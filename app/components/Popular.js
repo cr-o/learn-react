c009fd464087151090bbe21696d9a4ca45ac49b6
catbeans <workemailzoop@gmail.com>
2020-03-17 20:09:09 -0400

    Reusable Card Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
+
 function LanguagesNav({selected, onUpdateLanguage}){
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python' ]
     return(
@@ -31,18 +33,14 @@ function ReposGrid({repos}){
                 const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
                 const{ login, avatar_url } = owner
                 return (
-                    <li key={html_url} className ='card bg-light'>
-                        <h4 classNAme ='header-lg center-text'>
-                            #{index + 1}
-                        </h4>
-                        <img
-                            className ='avatar'
-                            src={avatar_url}
-                            alt={`Avatar for ${login}`}
-                        />
-                        <h2 className ='center-text'>
-                            <a className='link' href ={html_url}>{login}</a>
-                        </h2>
+                    <li key={html_url}>
+                        <Card
+                            header={`#${index + 1}`}
+                            // no subheader
+                            avatar={avatar_url}
+                            href={html_url}
+                            name={login}
+                        >
                         <ul className='card-list'>
                             <li>
                                 <FaUser color='rgb(255, 191, 116)' size={22} />
@@ -63,6 +61,7 @@ function ReposGrid({repos}){
                                 {open_issues.toLocaleString()} open
                             </li>
                         </ul>
+                        </Card>
                     </li>    
                 )
             })}
