07bee67f0ad937500ae4b449adeee744c3a02649
catbeans <workemailzoop@gmail.com>
2020-03-18 22:53:41 -0400

    Theme Provider

--- app/index.js
+++ app/index.js
@@ -3,17 +3,34 @@ import ReactDom from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 // Component
 // State
 // Lifecycle
 // UI
 
 class App extends React.Component{ // How to define a component
+    constructor(props){
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => { // put this on the state object itself
+                this.setState(({ theme })=>({ // functional setState since it depends on previous state
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            }  
+        }
+    }
     render(){
         return ( // JSX that will become JavaScript invocations by Bable
-            <div className ='container'>
-                <Battle />
-            </div>
+            // need to stick method on object we're going to pass in (toggletheme) as value on the ThemeProvider component
+            // wrapping entire app inside ThemePovider
+            <ThemeProvider value={this.state}>
+                <div className ='container'>
+                    <Battle />
+                </div>
+            </ThemeProvider>
         )
     }    
 }
