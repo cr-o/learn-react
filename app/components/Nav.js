b324c6a301cec628ffc6811df8a05cb071e7f63c
cat beans <workemailzoop@gmail.com>
2020-03-30 17:56:51 -0400

    Navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { ThemeConsumer } from '../contexts/theme'
+import { NavLink } from 'react-router-dom'
+// link works, but NavLink allows us to pass some extra props to link
+
+const activeStyle = {
+    color: 'rgb(187, 46, 31)'
+}
 
 export default function Nav(){
     return( // use render prop pattern
@@ -7,6 +13,27 @@ export default function Nav(){
             {/* because we wrapped it in Consumer, pass whatever we pass to provider's vcalue prop to this function */}
             {({theme, toggleTheme})=>(
                 <nav className='row space-between'>
+                    <ul className='row nav'>
+                        <li>
+                            <NavLink
+                                to='/'
+                                exact
+                                activeStyle={activeStyle}
+                                className='nav-link'
+                            >
+                                Popular
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to='/battle'
+                                activeStyle={activeStyle}
+                                className='nav-link'
+                            >
+                                Battle
+                            </NavLink>
+                        </li>
+                    </ul>
                     <button
                         style={{fontSize: 30}}
                         className='btn-clear'
